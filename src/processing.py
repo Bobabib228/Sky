5bda82ce38ad2b7b4395fcf404956cc095db672f
Bobabib228 <egor.tchichi@yandex.ru>
2026-06-10 21:28:43 +0300

    add test_processing

--- src/processing.py
+++ src/processing.py
@@ -24,3 +24,4 @@ def sort_by_date(data: list, reverse: bool = True) -> list:
         Новый отсортированный список словарей.
     """
     return sorted(data, key=lambda x: x.get('date', ''), reverse=reverse)
+
