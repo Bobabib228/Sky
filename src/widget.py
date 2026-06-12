de7bce3cd670b76454112a74678a74e737b81537
Bobabib228 <egor.tchichi@yandex.ru>
2026-06-12 18:50:36 +0300

    add linters widget.py

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,14 @@ import datetime
 
 
 def mask_account_card(input_number: str) -> str:
+    """
+    Маскирует номер банковского счета или карты в зависимости от типа.
+
+    Функция принимает строку, содержащую тип ('Счёт' или название карты) и номер.
+    Для счета: отображаются первые 4 символа и последние 4 цифры, остальное скрывается звездочками.
+    Для карты: отображаются название карты, первые 6 цифр (в формате XXXX XX** **** XXXX) и последние 4 цифры.
+
+    """
 
     typ = input_number.split()
 
@@ -12,8 +20,11 @@ def mask_account_card(input_number: str) -> str:
     else:
         Card_Name = typ[0]
         Card_Number = typ[1]
-        return f"{Card_Name} {Card_Number[0:4]} {Card_Number[4:6]}** **** {Card_Number[-4:]}"  # 1234 5678 9123 4567
+        return f"{Card_Name} {Card_Number[0:4]} {Card_Number[4:6]}** **** {Card_Number[-4:]}"
 
 
 def get_date(input_date: str) -> str:
+    """
+    Форматирует дату, приводя ее в формат ДД.ММ.ГГ
+    """
     return f"{input_date[8:10]}.{input_date[5:7]}.{input_date[0:4]}"
