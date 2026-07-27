e660536f30225b6b9bd574510d2f16d5a494b1fd
Bobabib228 <egor.tchichi@yandex.ru>
2026-07-27 22:07:52 +0300

    logging programm

---
+++ main.py
@@ -0,0 +1,4 @@
+from masks import *
+
+
+print(get_mask_card_number("1234567890123456"))
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 import json
+import logging
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs.log")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def load_transactions(file_path):
@@ -6,9 +14,12 @@ def load_transactions(file_path):
     Загружает транзакции из JSON-файла.
     Возвращает пустой список, если файл не найден, пустой или содержит не список.
     """
+    logger.info("Starting load_transactions")
     try:
         with open(file_path, "r") as f:
             data = json.load(f)
+            logger.info("Ending load_transactions")
             return data if isinstance(data, list) else []
-    except (FileNotFoundError, json.JSONDecodeError, AttributeError):
+    except (FileNotFoundError, json.JSONDecodeError, AttributeError) as e:
+        logger.error(e)
         return []
